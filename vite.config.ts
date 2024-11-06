7a1f1ac36c6f5362e548fddc218687912434dafb
vuvansy <vuvansy3001@gmail.com>
2024-11-06 23:42:16 +0700

    fix bug TypeScript Absolute Import

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import dns from 'dns'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import dns from "dns";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/server-options.html#server-options
-dns.setDefaultResultOrder('verbatim')
+dns.setDefaultResultOrder("verbatim");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
-})
-
+    plugins: [react(), tsconfigPaths()],
+    server: {
+        port: 3000,
+    },
+});
