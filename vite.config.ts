14d36c323054fec64a2137dcc10bf60c12742ae2
vuvansy <vuvansy3001@gmail.com>
2024-11-06 23:37:31 +0700

    TypeScript Absolute Import

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import dns from 'dns'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
 // https://vitejs.dev/config/server-options.html#server-options
 dns.setDefaultResultOrder('verbatim')
 
