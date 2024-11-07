61a4e5c9699b08a7e614e459f34fa7b6bd081c7d
vuvansy <vuvansy3001@gmail.com>
2024-11-07 13:47:33 +0700

    Hoàn thiện Project Screen

--- src/components/sections/project/project.card.tsx
+++ src/components/sections/project/project.card.tsx
@@ -4,22 +4,35 @@ import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
 
-function ProjectCard(props) {
+interface IProps {
+    imgPath: string;
+    title: string;
+    description: string;
+    githubLink: string;
+    demoLink: string;
+}
+function ProjectCard(props: IProps) {
     return (
         <Card className="project-card-view">
-            <Card.Img variant="top" src={props.imgPath} alt="card-img" style={{ maxHeight: 215 }} />
+            <Card.Img
+                variant="top"
+                src={props.imgPath}
+                alt="card-img"
+                style={{ maxHeight: 215 }}
+            />
             <Card.Body className="d-flex flex-column">
-                <Card.Title>
-                    {props.title}
-                </Card.Title>
+                <Card.Title>{props.title}</Card.Title>
                 <div className="d-flex flex-column justify-content-between h-100">
                     <Card.Text style={{ textAlign: "justify" }}>
                         {props.description}
                     </Card.Text>
                     <div>
-                        <Button variant="primary" href={props.githubLink} target="_blank">
-                            <BsGithub /> &nbsp;
-                            {props.isBlog ? "Blog" : "GitHub"}
+                        <Button
+                            variant="primary"
+                            href={props.githubLink}
+                            target="_blank"
+                        >
+                            <BsGithub /> &nbsp; GitHub
                         </Button>
 
                         <Button
@@ -31,7 +44,6 @@ function ProjectCard(props) {
                             <CgWebsite /> &nbsp;
                             {"Demo"}
                         </Button>
-
                     </div>
                 </div>
             </Card.Body>
